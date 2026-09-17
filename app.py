db6ab6ed4d45eb0cd34ed67f702cf51e48d92712
24120065-hue <24120065@student.bdu.edu.vn>
2026-09-17 16:06:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.image("logo1.jpg")
+st.image("logo1.JPG")
 import pandas as pd
 from datetime import datetime
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
  
-st.title("🍽️ Hệ thống Order Nhà Hàng - VŨ ĐỨC BÌNH")
+st.title("🍽️ Hệ thống Order Nhà Hàng - Minh Thư")
  
 # ==========================
 # Khởi tạo dữ liệu
